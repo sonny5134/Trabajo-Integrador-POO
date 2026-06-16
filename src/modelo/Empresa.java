7d8af3d05732b1bbb14cd3089c66af61b41c5e9e
thomilopez <thomasignaciolopez@gmail.com>
2026-06-15 23:02:54 -0300

    Creacion clase Empresa

--- src/modelo/Empresa.java
+++ src/modelo/Empresa.java
@@ -1,5 +1,21 @@
 package modelo;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Empresa {
-  
+    private String razonSocial;
+    private List<Empleado> empleados;
+
+    public Empresa(String razonSocial) {
+        this.razonSocial = razonSocial;
+        this.empleados = new ArrayList<>();
+    }
+
+    public void altaEmpleado(Empleado emp) {
+        this.empleados.add(emp);
+    }
+
+    public List<Empleado> obtenerTodosLosEmpleados() {
+        return this.empleados;
+    }
 }
